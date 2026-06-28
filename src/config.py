40beebcf6e3610fbdf04570286e713676173f20d
Sarang Dave <131434471+S84v@users.noreply.github.com>
2026-06-28 19:31:28 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,4 +1,13 @@
+# Paths
+DATA_DIR = "../data"
+
+# Image
 IMAGE_SIZE = 224
+
+# Training
 BATCH_SIZE = 32
 NUM_WORKERS = 0
-DATA_DIR = "../data"
+
+# ImageNet normalization params
+IMAGENET_MEAN = [0.485, 0.456, 0.406]
+IMAGENET_STD = [0.229, 0.224, 0.225]
