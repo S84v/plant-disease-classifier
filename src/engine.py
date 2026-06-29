67d2e5917f70b65532cb00d4946d09e759c372b0
Sarang Dave <131434471+S84v@users.noreply.github.com>
2026-06-29 21:03:18 +0530

    Add train, val and main block logic

--- src/engine.py
+++ src/engine.py
@@ -1,1 +1,95 @@
 # Train/validation logic
+
+import torch
+from torch import nn
+
+
+def train_one_epoch(model, dataloader, criterion, optimizer):
+    model.train()
+
+    running_loss = 0.0
+    running_correct = 0
+    total_samples = 0
+
+    for xb, yb in dataloader:
+
+        optimizer.zero_grad()
+        logits = model(xb)
+        loss = criterion(logits, yb)
+
+        loss.backward()
+        optimizer.step()
+
+        predictions = torch.argmax(logits, dim=1)
+
+        running_loss += loss.item() * xb.size(0)
+        running_correct += (predictions == yb).sum().item()
+        total_samples += xb.size(0)
+
+    epoch_loss = running_loss / total_samples
+    epoch_accuracy = running_correct / total_samples
+
+    return {"loss": epoch_loss, "accuracy": epoch_accuracy}
+
+
+def validate_one_epoch(model, dataloader, criterion):
+    model.eval()
+
+    running_loss = 0.0
+    running_correct = 0
+    total_samples = 0
+
+    with torch.no_grad():
+        for xb, yb in dataloader:
+            logits = model(xb)
+            loss = criterion(logits, yb)
+
+            predictions = torch.argmax(logits, dim=1)
+
+            running_loss += loss.item() * xb.size(0)
+            running_correct += (predictions == yb).sum().item()
+            total_samples += xb.size(0)
+        epoch_loss = running_loss / total_samples
+        epoch_accuracy = running_correct / total_samples
+
+    return {"loss": epoch_loss, "accuracy": epoch_accuracy}
+
+
+if __name__ == "__main__":
+    from data import create_dataloaders
+    from model import build_model
+    from torch import nn
+    from torch.optim import Adam
+
+    # Create dataloaders
+    train_loader, valid_loader, class_names = create_dataloaders()
+
+    # Build model
+    model = build_model(
+        num_classes=len(class_names), pretrained=True, freeze_backbone=True
+    )
+
+    # Define loss function and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = Adam(model.parameters(), lr=1e-3)
+
+    # Train for one epoch
+    train_metrics = train_one_epoch(
+        model=model,
+        dataloader=train_loader,
+        criterion=criterion,
+        optimizer=optimizer,
+    )
+
+    print("Training Metrics")
+    print(train_metrics)
+
+    # Validate for one epoch
+    valid_metrics = validate_one_epoch(
+        model=model,
+        dataloader=valid_loader,
+        criterion=criterion,
+    )
+
+    print("\nValidation Metrics")
+    print(valid_metrics)
