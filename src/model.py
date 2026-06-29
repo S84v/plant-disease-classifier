f363a3262cce9e090856b55e0c27b9886d7c4738
Sarang Dave <131434471+S84v@users.noreply.github.com>
2026-06-29 19:37:48 +0530

    Added docstring and comments

--- src/model.py
+++ src/model.py
@@ -5,34 +5,45 @@ from torch import nn
 
 
 def build_model(num_classes, pretrained=True, freeze_backbone=True):
+    """
+    Build a ResNet18 model for image classification.
 
+    Args:
+        num_classes: Number of output classes.
+        pretrained: Load ImageNet pretrained weights.
+        freeze_backbone: Freeze all backbone layers. If False,
+            only layer4 and the classifier remain trainable.
+
+    Returns:
+        Configured PyTorch model.
+    """
     weights = ResNet18_Weights.DEFAULT if pretrained else None
 
     model = resnet18(weights=weights)
     in_features = model.fc.in_features
 
-    model.fc = nn.Linear(in_features, num_classes)
+    model.fc = nn.Linear(in_features, num_classes) # Change final layer to (512, 38)
 
     if pretrained and freeze_backbone:
 
-        for param in model.parameters():
+        for param in model.parameters(): # Freeze entire model
             param.requires_grad = False
 
-        for param in model.fc.parameters():
+        for param in model.fc.parameters(): # Unfreeze fc layer
             param.requires_grad = True
 
     elif pretrained and not freeze_backbone:
 
-        for param in model.parameters():
+        for param in model.parameters(): # Freeze entire model
             param.requires_grad = False
 
-        for param in model.layer4.parameters():
+        for param in model.layer4.parameters(): # Unfreeze layer 4
             param.requires_grad = True
 
-        for param in model.fc.parameters():
+        for param in model.fc.parameters(): # Unfreeze fc layer
             param.requires_grad = True
 
-    else:
+    else: # Freeze nothing. Train all layers.
         pass
 
     return model
