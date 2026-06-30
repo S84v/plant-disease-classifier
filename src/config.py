55e329a28bb2a7d4af8cde102f8b37996e92527b
Sarang Dave <131434471+S84v@users.noreply.github.com>
2026-06-30 20:08:01 +0530

    Add parameters for model training

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 # Paths
 DATA_DIR = "../data"
+MODEL_DIR = "../models"
 
 # Image
 IMAGE_SIZE = 224
@@ -7,6 +8,8 @@ IMAGE_SIZE = 224
 # Training
 BATCH_SIZE = 32
 NUM_WORKERS = 0
+EPOCHS = 5
+LR = 1e-3
 
 # ImageNet normalization params
 IMAGENET_MEAN = [0.485, 0.456, 0.406]
