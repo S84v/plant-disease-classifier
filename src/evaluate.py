f0dd9414eff048f3458d4609c17dbbde76353410
Sarang Dave <131434471+S84v@users.noreply.github.com>
2026-06-30 21:13:35 +0530

    Add functionality to load model from disk

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,1 +1,23 @@
 # Model evaluation logic
+import config
+from data import create_dataloaders
+from model import build_model
+
+import torch
+from torch import nn
+
+from sklearn import metrics
+import matplotlib.pyplot as plt
+import numpy as np
+
+from pathlib import Path
+
+
+def load_model():
+    model = build_model(config.NUM_CLASSES, pretrained=True, freeze_backbone=False)
+    model.load_state_dict(torch.load(Path(config.MODEL_DIR) / "best_model.pth"))
+    model.eval()
+
+    return model
+
+
