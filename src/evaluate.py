1bee434145f6895d0fe68f854c8174a0af6c99f1
Sarang Dave <131434471+S84v@users.noreply.github.com>
2026-06-30 21:23:26 +0530

    Add logic to compute and return metrics

--- src/evaluate.py
+++ src/evaluate.py
@@ -39,3 +39,19 @@ def collect_predictions(model, dataloader, loss_fn):
         avg_loss = running_loss / total_samples
 
     return avg_loss, y_true, y_pred
+
+
+def compute_metrics(y_true, y_pred):
+    accuracy = metrics.accuracy_score(y_true, y_pred)
+    precision = metrics.precision_score(y_true, y_pred)
+    recall = metrics.recall_score(y_true, y_pred)
+    f1 = metrics.f1_score(y_true, y_pred)
+    class_report = metrics.classification_report(y_true, y_pred)
+
+    return {
+        "Accuracy score": accuracy,
+        "Precision score": precision,
+        "Recall score": recall,
+        "F1 Score": f1,
+        "Classification Report": class_report,
+    }
