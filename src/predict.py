84f35d592acf3fa9498d567f4d791c9e87113dc8
Sarang Dave <131434471+S84v@users.noreply.github.com>
2026-07-02 18:20:21 +0530

    Add imports

--- src/predict.py
+++ src/predict.py
@@ -1,1 +1,11 @@
 # Load a trained model and perform inference on unseen images
+
+import config
+from model import build_model
+
+import torch
+from torchvision.transforms import v2
+from PIL import Image
+import pathlib
+import matplotlib.pyplot as plt
+
