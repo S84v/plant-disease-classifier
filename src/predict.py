f68f743ccca3c0f07eb5e8e38a7c9ad7ba7b9b3c
Sarang Dave <131434471+S84v@users.noreply.github.com>
2026-07-02 18:23:50 +0530

    Add logic to load model

--- src/predict.py
+++ src/predict.py
@@ -6,6 +6,18 @@ from model import build_model
 import torch
 from torchvision.transforms import v2
 from PIL import Image
-import pathlib
+from pathlib import Path
 import matplotlib.pyplot as plt
 
+
+def load_model():
+    model = build_model(
+        num_classes=config.NUM_CLASSES, pretrained=True, freeze_backbone=False
+    )
+    model.load_state_dict(
+        state_dict=torch.load(Path(config.MODEL_DIR) / "best_model.pth")
+    )
+
+    model.eval()
+
+    return model
