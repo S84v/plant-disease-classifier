db045a9626e4948e93a38398201906957b1c83b8
Sarang Dave <131434471+S84v@users.noreply.github.com>
2026-07-03 18:12:22 +0530

    Add logic to load model and return predictions

--- src/api.py
+++ src/api.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException, Request
 from contextlib import asynccontextmanager
 from predict import load_model, get_inference_transform, preprocess_image, predict_image
+import config
 from io import BytesIO
 from PIL import Image, UnidentifiedImageError
 
@@ -10,6 +11,7 @@ from PIL import Image, UnidentifiedImageError
 async def lifespan(app: FastAPI):
     print("Loading model...")
     app.state.model = load_model()
+    app.state.transform = get_inference_transform()
     print("Model loaded successfully.")
     yield
     print("Shutting down API...")
@@ -34,12 +36,15 @@ def health_check():
 
 
 @app.post("/predict")
-async def predict(file: UploadFile = File(...)):
+async def predict(request: Request, file: UploadFile = File(...)):
     allowed_types = {
         "image/jpeg",
         "image/png",
         "image/webp",
     }
+
+    model = request.app.state.model
+    transform = request.app.state.transform
 
     if file.content_type not in allowed_types:
         raise HTTPException(status_code=415, detail="Unsupported image format.")
@@ -50,10 +55,15 @@ async def predict(file: UploadFile = File(...)):
         image = Image.open(BytesIO(image_bytes))
         image.load()
         image = image.convert("RGB")
+
+        image_tensor = preprocess_image(image=image, transform=transform)
+        prediction = predict_image(
+            model=model,
+            image_tensor=image_tensor,
+            class_names=config.CLASS_NAMES,
+            top_k=5,
+        )
+
     except UnidentifiedImageError:
         raise HTTPException(status_code=400, detail="Invalid image file.")
-    return {
-        "predicted_class": None,
-        "confidence": None,
-        "top_predictions": [],
-    }
+    return prediction
